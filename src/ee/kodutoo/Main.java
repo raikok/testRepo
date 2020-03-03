c2376be2a054f02616330fa4c4b21863c8e148e9
raikok <56067766+raikok@users.noreply.github.com>
2020-03-03 23:34:48 +0200

    goodbye

--- src/ee/kodutoo/Main.java
+++ src/ee/kodutoo/Main.java
@@ -3,6 +3,6 @@ package ee.kodutoo;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Goodbye world!");
     }
 }
